6ad2e64e57c0f4795a1edb58817113f47427b1fd
THEdie17 <dieguetefernansanz@gmail.com>
2023-03-01 19:53:27 +0100

    register

--- app/src/main/java/com/example/jankenultra/Splash.kt
+++ app/src/main/java/com/example/jankenultra/Splash.kt
@@ -3,22 +3,29 @@ package com.example.jankenultra
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class Splash : AppCompatActivity() {
-    private val duracio: Long=3000;
+    private val duration: Long=10000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         //amaguem la barra, pantalla a full
         supportActionBar?.hide()
-        canviarActivity();
+        //cridem a la funció de canviar activitat
+        changeActivity()
     }
-    private fun canviarActivity(){
-        Handler().postDelayed({
-            val intent= Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }, duracio)
+    private fun changeActivity(){
+        Timer().schedule(duration){
+            jumpStart()
+        }
+    }
+    private fun jumpStart()
+    {
+        val intent=Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
+
 
