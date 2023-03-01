bb4e389303e1ae8887d2dbbca416cf269b43083b
isanchezIESCastellet <isanchezf@inscastellet.cat>
2023-03-01 18:47:47 +0100

    Register hecho

--- app/src/main/java/com/example/jankenultra/MainActivity.kt
+++ app/src/main/java/com/example/jankenultra/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.jankenultra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loginBtn = findViewById<Button>(R.id.LOGINBTN)
+        val registerBtn = findViewById<Button>(R.id.REGISTERBTN)
+        loginBtn.setOnClickListener{
+            Toast.makeText(this, "Click Login Button",Toast.LENGTH_LONG).show()
+        }
+        registerBtn.setOnClickListener{
+            Toast.makeText(this, "Click Register Button",Toast.LENGTH_LONG).show()
+        }
     }
 }
