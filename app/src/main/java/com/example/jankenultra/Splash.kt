17265c25f4fd162df52f661182c4d400518ff58e
THEdie17 <dieguetefernansanz@gmail.com>
2023-03-01 15:21:25 +0100

    Splash a la mitad

---
+++ app/src/main/java/com/example/jankenultra/Splash.kt
@@ -0,0 +1,24 @@
+package com.example.jankenultra
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+    private val duracio: Long=3000;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //amaguem la barra, pantalla a full
+        supportActionBar?.hide()
+        canviarActivity();
+    }
+    private fun canviarActivity(){
+        Handler().postDelayed({
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }, duracio)
+    }
+}
+
