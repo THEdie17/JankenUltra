46c0f4ee9437224df06dc3f7bae4c9111363bc9c
THEdie17 <dieguetefernansanz@gmail.com>
2023-03-10 15:59:14 +0100

    LoginBackgroundMusic

--- app/src/main/java/com/example/jankenultra/Login.kt
+++ app/src/main/java/com/example/jankenultra/Login.kt
@@ -3,6 +3,7 @@ package com.example.jankenultra
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
@@ -12,35 +13,74 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import java.text.SimpleDateFormat
 import java.util.*
+
+
+
+
 class Login : AppCompatActivity() {
+    lateinit var correoLogin : EditText
+    lateinit var passLogin : EditText
+    lateinit var login : Button
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         //Despleguem les variables que farem servir
-        lateinit var correoLogin : EditText
-        lateinit var passLogin : EditText
-        lateinit var BtnLogin : Button
+        correoLogin = findViewById<EditText>(R.id.emailLogin)
+        passLogin = findViewById<EditText>(R.id.passLogin)
+        auth = FirebaseAuth.getInstance()
+        if (auth == null){
+            Log.i("LOGIN_OBJ","nulo")
+        }
 
-        BtnLogin.setOnClickListener(){
+        login = findViewById<Button>(R.id.login)
+        login.setOnClickListener() {
             //Abans de fer el registre validem les dades
-            var email:String = correoLogin.getText().toString()
-            var passw:String = passLogin.getText().toString()
+            var email: String = correoLogin.text.toString()
+            var passw: String = passLogin.text.toString()
             // validació del correu
             // si no es de tipus correu
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                correoLogin.setError("Invalid Mail")
-            }
-            else if (passw.length<6) {
-                passLogin.setError("Password less than 6 chars")
-            }else{
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                correoLogin.error = "Invalid Mail"
+            } else if (passw.length < 6) {
+                passLogin.error = "Password less than 6 chars"
+            } else {
                 // aquí farem LOGIN al jugador
+                Log.i("LOGIN_OBJ",email)
+                Log.i("LOGIN_OBJ",passw)
+                if (auth != null){
+                    Log.i("LOGIN_OBJ","nulo")
+                }
                 LogindeJugador(email, passw)
             }
         }
     }
 
     private fun LogindeJugador(email: String, passw: String) {
-        auth.signInWithEmailAndPassword(email,passw)
+        Toast.makeText(this, "hola", Toast.LENGTH_SHORT).show()
+        auth.signInWithEmailAndPassword(email, passw)
+            .addOnCompleteListener(this)
+            { task ->
+                if (task.isSuccessful) {
+                    val tx: String = "Benvingut " + email
+                    Toast.makeText(this, tx, Toast.LENGTH_LONG).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+
+                } else {
+                    Toast.makeText(
+                        this, "ERROR Autentificació",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    Log.i("LOGIN_OBJ","NO FUNCIONA")
+                }
+            }
+    }
+
+    fun updateUI(user: FirebaseUser?) {
+        val intent = Intent(this, Menu::class.java)
+        startActivity(intent)
         finish()
     }
+
 }
--- app/src/main/java/com/example/jankenultra/Splash.kt
+++ app/src/main/java/com/example/jankenultra/Splash.kt
@@ -1,6 +1,7 @@
 package com.example.jankenultra
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.util.Timer
@@ -8,9 +9,13 @@ import kotlin.concurrent.schedule
 
 class Splash : AppCompatActivity() {
     private val duration: Long=10000
+    lateinit var mp: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        mp = MediaPlayer.create(this,R.raw.splashmusic)
+        mp.setOnPreparedListener{ mp.start() }
         //amaguem la barra, pantalla a full
         supportActionBar?.hide()
         //cridem a la funció de canviar activitat
@@ -21,9 +26,9 @@ class Splash : AppCompatActivity() {
             jumpStart()
         }
     }
-    private fun jumpStart()
-    {
-        val intent=Intent(this, MainActivity::class.java)
+    private fun jumpStart() {
+        val intent = Intent(this, MainActivity::class.java)
+        mp.release()
         startActivity(intent)
     }
 }
