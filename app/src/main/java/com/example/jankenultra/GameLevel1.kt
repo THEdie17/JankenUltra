a03ca434851eb5deb30d674ccc2338772ace5a7d
THEdie17 <81924547+THEdie17@users.noreply.github.com>
2023-03-18 20:21:19 +0100

    level1Base

--- app/src/main/java/com/example/jankenultra/GameLevel1.kt
+++ app/src/main/java/com/example/jankenultra/GameLevel1.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlin.random.Random
 
 class GameLevel1 : AppCompatActivity() {
 
@@ -19,10 +20,62 @@ class GameLevel1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gamelevel1)
 
+        val arr = Array(3) { "Rock";"Paper";"Scissors" }
+        var userChoice = -1
+        var userWins = 0
+        var machineChoice = -1
+        var MachineWins = 0
+        var victory = false
+
+
+
         exit = findViewById<Button>(R.id.button)
         exit.setOnClickListener{
             val intent= Intent(this, choseLevel::class.java)
             startActivity(intent)
         }
+
+        rock = findViewById<Button>(R.id.buttonRock)
+        rock.setOnClickListener{
+            userChoice = 0
+        }
+
+        paper = findViewById<Button>(R.id.buttonPaper)
+        paper.setOnClickListener{
+            userChoice = 1
+        }
+
+        scissors = findViewById<Button>(R.id.buttonScissors)
+        scissors.setOnClickListener{
+            userChoice = 2
+        }
+        if (userChoice > -1){
+            machineChoice = Random.nextInt(0, 3)
+
+            "Rock0 paper1 scissor2"
+            if (userChoice == 0 && machineChoice == 2){
+                userWins += 1
+                Toast.makeText(this,"Round Win", Toast.LENGTH_SHORT).show()
+            }else if (userChoice == 0 && machineChoice == 1){
+                MachineWins += 1
+                Toast.makeText(this,"Round Lose", Toast.LENGTH_SHORT).show()
+            }
+
+            if (userChoice == 1 && machineChoice == 0){
+                userWins += 1
+                Toast.makeText(this,"Round Win", Toast.LENGTH_SHORT).show()
+            }else if (userChoice == 1 && machineChoice == 2){
+                MachineWins += 1
+                Toast.makeText(this,"Round Lose", Toast.LENGTH_SHORT).show()
+            }
+            if (userChoice == 1 && machineChoice == 0){
+                userWins += 1
+                Toast.makeText(this,"Round Win", Toast.LENGTH_SHORT).show()
+            }else if (userChoice == 1 && machineChoice == 2){
+                MachineWins += 1
+                Toast.makeText(this,"Round Lose", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
