bfb2056d82ba3204f909f2e8f3237247ce78ee2f
isanchezIESCastellet <isanchezf@inscastellet.cat>
2023-03-24 18:47:42 +0100

    Levels 1 Adapted

--- app/src/main/java/com/example/jankenultra/GameLevel1.kt
+++ app/src/main/java/com/example/jankenultra/GameLevel1.kt
@@ -1,9 +1,12 @@
 package com.example.jankenultra
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -14,87 +17,85 @@ class GameLevel1 : AppCompatActivity() {
     private lateinit var paper : ImageButton
     private lateinit var scissors : ImageButton
     private lateinit var exit : Button
-    val arr = Array(3) { "Rock";"Paper";"Scissors" }
-    var userChoice = -1
-    var userWins = 0
-    var machineChoice = -1
-    var MachineWins = 0
-    var hasFinished = false
+    private lateinit var userChoiceImg : ImageView
+    private lateinit var machineChoiceImg : ImageView
+    private lateinit var scoreLevel : TextView
+    private val arr = arrayOf("Rock", "Paper", "Scissors")
+    private var userChoice = -1
+    private var userWins = 0
+    private var machineChoice = -1
+    private var machineWins = 0
+    private var hasFinished = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gamelevel1)
-
-
-
-        exit = findViewById<Button>(R.id.button)
+        scoreLevel = findViewById(R.id.scoreLevel1)
+        userChoiceImg = findViewById(R.id.UserChoice)
+        machineChoiceImg = findViewById(R.id.MachineChoice)
+        exit = findViewById(R.id.button)
         exit.setOnClickListener{
             val intent= Intent(this, ChooseLevel::class.java)
             startActivity(intent)
         }
 
-        rock = findViewById<ImageButton>(R.id.buttonRock)
+        rock = findViewById(R.id.buttonRock)
         rock.setOnClickListener{
             userChoice = 0
-            Toast.makeText(this," -"+userChoice, Toast.LENGTH_SHORT).show()
-            comprobarButton()
+            checkButton()
         }
 
-        paper = findViewById<ImageButton>(R.id.buttonPaper)
+        paper = findViewById(R.id.buttonPaper)
         paper.setOnClickListener{
             userChoice = 1
-            Toast.makeText(this," -"+userChoice, Toast.LENGTH_SHORT).show()
-            comprobarButton()
+            checkButton()
         }
 
-        scissors = findViewById<ImageButton>(R.id.buttonScissors)
+        scissors = findViewById(R.id.buttonScissors)
         scissors.setOnClickListener{
             userChoice = 2
-            Toast.makeText(this," -"+userChoice, Toast.LENGTH_SHORT).show()
-            comprobarButton()
+            checkButton()
         }
     }
 
-    private fun comprobarButton() {
+    private fun checkButton() {
         machineChoice = Random.nextInt(0, 3)
 
-        "Rock0 paper1 scissor2"
-        if (userChoice == 0 && machineChoice == 2){
-            userWins += 1
-            Toast.makeText(this,"Round Win", Toast.LENGTH_SHORT).show()
-        }else if (userChoice == 0 && machineChoice == 1){
-            MachineWins += 1
-            Toast.makeText(this,"Round Lose", Toast.LENGTH_SHORT).show()
-        }else if (userChoice == 1 && machineChoice == 0){
-            userWins += 1
-            Toast.makeText(this,"Round Win", Toast.LENGTH_SHORT).show()
-        }else if (userChoice == 1 && machineChoice == 2){
-            MachineWins += 1
-            Toast.makeText(this,"Round Lose", Toast.LENGTH_SHORT).show()
-        }else if (userChoice == 2 && machineChoice == 1){
-            userWins += 1
-            Toast.makeText(this,"Round Win", Toast.LENGTH_SHORT).show()
-        }else if (userChoice == 2 && machineChoice == 0){
-            MachineWins += 1
-            Toast.makeText(this,"Round Lose", Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(this,"machine: "+machineChoice, Toast.LENGTH_SHORT).show()
-        }
-        userChoice = -1
+        // Show the user's and machine's choices
+        userChoiceImg.setImageResource(getDrawableResourceId(arr[userChoice]))
+        machineChoiceImg.setImageResource(getDrawableResourceId(arr[machineChoice]))
 
-        if (userWins == 3){
+        // Check the result of the round
+        when (userChoice) {
+            machineChoice -> Toast.makeText(this, "Round Draw", Toast.LENGTH_SHORT).show()
+            0 -> if (machineChoice == 2) userWins++ else machineWins++
+            1 -> if (machineChoice == 0) userWins++ else machineWins++
+            2 -> if (machineChoice == 1) userWins++ else machineWins++
+        }
+
+        // Show the score
+        Toast.makeText(this, " wins $userWins", Toast.LENGTH_SHORT).show()
+
+        // Check if the game is over
+        if (userWins == 3) {
             Toast.makeText(this,"VICTORY", Toast.LENGTH_SHORT).show()
             hasFinished = true
             val intent= Intent(this, Menu::class.java)
             startActivity(intent)
-        }else if (MachineWins == 3){
+        } else if (machineWins == 3) {
             Toast.makeText(this,"LOSE", Toast.LENGTH_SHORT).show()
             hasFinished = true
             val intent= Intent(this, Menu::class.java)
             startActivity(intent)
         }
 
-        Toast.makeText(this," wins "+userWins, Toast.LENGTH_SHORT).show()
+        // Reset userChoice
+        userChoice = -1
+    }
+
+    @SuppressLint("DiscouragedApi")
+    private fun getDrawableResourceId(name: String): Int {
+        return resources.getIdentifier(name.lowercase(), "drawable", packageName)
     }
 
 }
